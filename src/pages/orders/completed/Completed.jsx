a0f3041a3c65059405e2aa4d8e5b34e1e42d59c0
josephmusya <muciajoe@gmail.com>
2024-03-11 11:37:49 +0300

    Fix: Styling

--- src/pages/orders/completed/Completed.jsx
+++ src/pages/orders/completed/Completed.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./completed.css";
 import OrderComponent from "../../../components/order-component/OrderComponent";
 import { useOrderContext } from "../../../providers/OrderProvider";
-import { HiMiniClipboardDocumentList } from "react-icons/hi2";
 import LoadingSkeletonOrder from "../../loading/Loading";
 import { useNavigate } from "react-router-dom";
 import ViewMore from "../../../components/more/ScrollMore";
+import OrderPlaceholder from "../../../components/order-placeholder/OrderPlaceholder";
+
 const Completed = () => {
   const navigate = useNavigate();
 
@@ -23,15 +24,7 @@ const Completed = () => {
       {ordersCompleted.next && <ViewMore fetch={getCompleted} />}
     </>
   ) : (
-    <div className="create-task-div">
-      <div className="child">
-        <article>Find orders you complete here</article>
-        <HiMiniClipboardDocumentList size={120} className="placeholder-icon" />
-        <article className="go-to-order" onClick={() => navigate("/app")}>
-          Go to Orders
-        </article>
-      </div>
-    </div>
+    <OrderPlaceholder message="Orders completed will appear here" />
   );
 };
 
